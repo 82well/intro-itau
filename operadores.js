5d0786efeb7ccc4e7df8756181bd9eb6ea1d601e
Douglas Morais <mr.douglasmorais23@gmail.com>
2023-01-03 21:45:02 -0300

    Operadores

---
+++ operadores.js
@@ -0,0 +1,13 @@
+const valueCalc = 2 > 3
+
+const sumValue = 12 + 5 + 10
+
+//  + soma
+// - sub
+// / divisão
+// * multiplicação
+// < ou > comparação
+// === estritamente
+// == é igual
+// !== estritamente diferente diferente
+// != diferente
