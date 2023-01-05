b04642a3ac64a0774858e530c7f58e13813f28ff
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2023-01-04 21:05:33 -0300

    create countdown

---
+++ contador-regressivo/script.js
@@ -0,0 +1,19 @@
+function startTimer(duration, display){
+  var timer = duration, minutes, seconds;
+  setInterval(function(){
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    display.textContent = minutes + ":" + seconds
+    if(--timer < 0){
+      timer = duration
+    }
+  }, 1000);
+}
+
+window.onload = function () {
+  var duration = 60 * 5 //converte para segundos
+  display = document.querySelector('#timer'); // Seleciona a div timer
+  startTimer(duration, display)
+}
